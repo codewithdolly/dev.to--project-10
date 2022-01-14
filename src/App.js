9f17d3de458822a6b42a0739f07ca39c0997f241
hicoderindia <hicoder.dolly@gmail.com>
2022-01-14 20:19:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,51 +5,9 @@ import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import Badge from '@mui/material/Badge';
 import Logo from "./Components/Images/logo.png"
 import Dolly from "./Components/Images/dolly.jpg"
-import { styled, alpha } from '@mui/material/styles';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import Avatar from '@mui/material/Avatar';
 import SearchIcon from '@mui/icons-material/Search';
-import InputBase from '@mui/material/InputBase';
-
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
 
 const App = () => {
   return (
@@ -65,19 +23,10 @@ const App = () => {
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
-  <Search className='form-inline m-2 my-lg-0 collapse navbar-collapse'>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-  {/* <form className="form-inline m-2 my-lg-0 collapse navbar-collapse">     
+  <form className="form-inline m-2 my-lg-0 collapse navbar-collapse">     
        <input className="form-control mr-sm-2 pr-3" type="search" placeholder="Search" aria-label="Search" />
 
-       </form> */}
+       </form>
 
     
    <div className='mr-5'>
