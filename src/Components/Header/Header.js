d0d156c55553eb9291fb2585915c868c25218e30
hicoderindia <hicoder.dolly@gmail.com>
2022-01-16 00:31:27 +0530

    Header updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -48,7 +48,7 @@ const SearchIconWrapper = styled("div")(({ theme }) => ({
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
   "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
+    padding: theme.spacing(0, 1, 1, 0),
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     transition: theme.transitions.create("width"),
     width: "100%",
