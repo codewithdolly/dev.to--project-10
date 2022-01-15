c914487d9b9325a6dbe2267f42c2cb2d7b9f022c
hicoderindia <hicoder.dolly@gmail.com>
2022-01-16 02:37:21 +0530

    Update LeftSidebar.js

--- src/Components/LeftSidebar/LeftSidebar.js
+++ src/Components/LeftSidebar/LeftSidebar.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import "./LeftSidebar.scss"
+import React, {useState} from 'react';
+import "./LeftSidebar.scss";
+import InputEmoji from 'react-input-emoji'
 
 const LeftSidebar = () => {
+    const [ text, setText ] = useState('')
+
+    function handleOnEnter (text) {
+        console.log('enter', text)
+      }
     return (
         <div>
-            {/* <h1>LeftSidebar</h1> */}
+             <InputEmoji
+          value={text}
+          onChange={setText}
+          cleanOnEnter
+          onEnter={handleOnEnter}
+          placeholder="Type a message"
+        />
         </div>
     )
 }
