5aea3ae5e3470e1f95ac266e9fe3089a1ac1ce67
hicoderindia <hicoder.dolly@gmail.com>
2022-01-15 23:13:23 +0530

    w

--- src/Components/DevPodcast/DevPodcast.js
+++ src/Components/DevPodcast/DevPodcast.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import "./DevPodcast.scss"
+import React from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./DevPodcast.scss";
 
 const DevPodcast = () => {
-    return (
-        <div>
-            DevPodcast
-        </div>
-    )
-}
+  return (
+    <>
+      <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>Holy guacamole!</strong> You should check in on some of those
+        fields below.
+        <button
+          type="button"
+          class="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default DevPodcast
+export default DevPodcast;
