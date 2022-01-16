42b55bff233614bb131b940ba20fc72099630468
hicoderindia <hicoder.dolly@gmail.com>
2022-01-16 16:46:54 +0530

    Sidebar

--- src/Components/Bootstrap/Header.js
+++ src/Components/Bootstrap/Header.js
@@ -45,3 +45,6 @@ const App = () => {
 }
 
 export default App
+
+
+// 🏠📚📜🎙️🎥📝💡🛍️❤️💻🖼️👍🤓👀👇🎉
--- src/Components/LeftSidebar/LeftSidebar.js
+++ src/Components/LeftSidebar/LeftSidebar.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React from "react";
 import "./LeftSidebar.scss";
-import { TwoWheeler } from '@mui/icons-material';
-import { TwoWheelerOutlined } from '@mui/icons-material';
+import { TwoWheeler } from "@mui/icons-material";
+import { TwoWheelerOutlined } from "@mui/icons-material";
 
 const LeftSidebar = () => {
-
-    return (
-        <div>
-        <TwoWheelerOutlined />
-            {/* <TwemojiText>
-            Hello world! 🌎
-        </TwemojiText> */}
-        </div>
-    )
-}
+  return (
+    <div>
+      <TwoWheelerOutlined />
+     
+    </div>
+  );
+};
 
 export default LeftSidebar;
+
+const NavLists = [
+  {
+    icon: "🏠",
+    name: "Home",
+  },
+  {
+    icon: "📚",
+    name: "Reading List",
+  },
+  {
+    icon: "📜",
+    name: "Listing",
+  },
+  {
+    icon: "🎙️",
+    name: "Podcasts",
+  },
+  {
+    icon: "🎥",
+    name: "ideos",
+  },
+  {
+    icon: "📝",
+    name: "Tags",
+  },
+  {
+    icon: "💡",
+    name: "FAQ",
+  },
+  {
+    icon: "🛍️",
+    name: "Dev Shop",
+  },
+  {
+    icon: "❤️",
+    name: "Sponsors",
+  },
+  {
+    icon: "💻",
+    name: "About",
+  },
+  {
+    icon: "🤝",
+    name: "Contact",
+  },
+  
+];
+
+
+
+const otherTag= [
+    {
+        icon: "👍",
+        name: "Code of Conduct",
+      },
+      {
+        icon: "🤓",
+        name: "Privacy Policy",
+      },
+      {
+        icon: "👀",
+        name: "Terms of use",
+      },
+]
