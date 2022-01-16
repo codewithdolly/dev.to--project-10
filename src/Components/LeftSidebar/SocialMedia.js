51842e767107c09a1eca28d23bb59f53cabb51fe
hicoderindia <hicoder.dolly@gmail.com>
2022-01-17 02:23:52 +0530

    Hashtags added

--- src/Components/LeftSidebar/SocialMedia.js
+++ src/Components/LeftSidebar/SocialMedia.js
@@ -8,15 +8,44 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import { Link } from "@mui/material";
 
 const SocialMedia = () => {
+    function handleChange(evt) {
+        console.log("new value is: ", evt.target.value);
+      }
   return (
     <>
-      <div className="d-flex justify-content-around socialMedia ">
-      {socialIcon.map((media)=>{
-          return(<>
-            <Link to={media.link}><div className="socialMedia--link">{media.icon}</div></Link>
-          </>)
-      })}
-       
+      <div className="socialMedia">
+        {/* Socail media page started */}
+        <div className="d-flex justify-content-around  my-3">
+          {" "}
+          {socialIcon.map((media) => {
+            return (
+              <>
+                <Link to={media.link}>
+                  <div className="socialMedia--link p-2">{media.icon}</div>
+                </Link>
+              </>
+            );
+          })}
+        </div>
+        {/* Socail media page ends */}
+
+        {/* hashtags started */}
+        <div>
+          <p>
+            <b>Populor Tags</b>
+          </p>
+          <select name="Software" id="software" multiple className="p-2" onChange={handleChange}>
+
+            {hashTags.map((tag) => {
+              return (
+                <>
+                  <option value={tag}>{tag}</option>
+                </>
+              );
+            })}
+          </select>
+        </div>
+        {/* hashtags ends */}
       </div>
     </>
   );
@@ -24,27 +53,38 @@ const SocialMedia = () => {
 
 export default SocialMedia;
 
-const socialIcon=[
-    {
-        icon:<TwitterIcon  />,
-        link:"https://www.linkedin.com/in/codewithdolly/",
-    },
-    {
-        icon:<FacebookIcon  />,
-        link:"https://www.linkedin.com/in/codewithdolly/",
-    },
-    {
-        icon:<GitHubIcon  />,
-        link:"https://www.linkedin.com/in/codewithdolly/",
-    },
-    {
-        icon:<LinkedInIcon  />,
-        link:"https://www.linkedin.com/in/codewithdolly/",
-    },
-    {
-        icon:<InstagramIcon  />,
-        link:"https://www.linkedin.com/in/codewithdolly/",
-    },
+const socialIcon = [
+  {
+    icon: <TwitterIcon />,
+    link: "https://www.linkedin.com/in/codewithdolly/",
+  },
+  {
+    icon: <FacebookIcon />,
+    link: "https://www.linkedin.com/in/codewithdolly/",
+  },
+  {
+    icon: <GitHubIcon />,
+    link: "https://www.linkedin.com/in/codewithdolly/",
+  },
+  {
+    icon: <LinkedInIcon />,
+    link: "https://www.linkedin.com/in/codewithdolly/",
+  },
+  {
+    icon: <InstagramIcon />,
+    link: "https://www.linkedin.com/in/codewithdolly/",
+  },
+];
 
-]
-
+const hashTags = [
+  "#html",
+  "#css",
+  "#JavaScript",
+  "#PHP",
+  "#docker",
+  "#Security",
+  "#Angular",
+  "#blockchain",
+  "#vue",
+  "#Lenux",
+];
