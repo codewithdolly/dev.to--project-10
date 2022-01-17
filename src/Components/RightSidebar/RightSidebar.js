60467314c67cbbfc0583f75ff9e7f3fee0ed7b4a
hicoderindia <hicoder.dolly@gmail.com>
2022-01-17 17:31:24 +0530

    right side bar started

--- src/Components/RightSidebar/RightSidebar.js
+++ src/Components/RightSidebar/RightSidebar.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./RightSidebar.scss"
+import React from "react";
+import "./RightSidebar.scss";
+import { Link } from "react-router-dom";
+import code from "../Images/code.png"
+
 
 const RightSidebar = () => {
-    return (
-        <div>
-            RightSidebar
+  return (
+    <>
+      <div className="rightSidebar">
+        <div className="card rightSidebar--card p-3" style={{width: "19rem"}}>
+          <img src={code} class="card-img-top" alt="Code Img" height="130px"  />
+          <div class="card-body">
+            <h5 class="card-title">CodeNewbie Challenge 2022</h5>
+            <h6 class="card-title"><b>|Write More|Start Coding|Code More|Get a Job|Learn in Public|<Link to="/"> → Join Cohort 1 by Jan 30th</Link></b></h6>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
