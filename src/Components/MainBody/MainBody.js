d2935918ef32c4e559ff577c1994e5495db63f39
hicoderindia <hicoder.dolly@gmail.com>
2022-01-19 03:25:25 +0530

    Update MainBody.js

--- src/Components/MainBody/MainBody.js
+++ src/Components/MainBody/MainBody.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./MainBody.scss";
+import { Link } from 'react-router-dom';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,11 +8,14 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import JSImg from "../Images/js.png";
+import { Avatar } from '@mui/material';
 
 const MainBody = () => {
     return (
         <>
         <div className="mainBody">
+        
+       
         <Card sx={{ maxWidth: 700}} className="mainBody--card">
       <CardMedia
         component="img"
@@ -20,9 +24,17 @@ const MainBody = () => {
         alt="green iguana"
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+      <div className='d-flex'>
+      <Avatar src={JSImg} className='mr-2' />
+        <Typography gutterBottom>
+        Tapas Adhikary <br /> jan 18
         </Typography>
+        </div>
+      <Link to="/">
+        <Typography gutterBottom variant="h4">
+        JavaScript object destructuring usages you must know
+        </Typography>
+        </Link>
         <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
