38bbff567356a3d4d4015a4abd44e76f7a3927e1
hicoderindia <hicoder.dolly@gmail.com>
2022-01-19 12:12:41 +0530

    up

--- src/Components/MainBody/MainBody.js
+++ src/Components/MainBody/MainBody.js
@@ -1,54 +1,53 @@
-import React from 'react';
+import React from "react";
 import "./MainBody.scss";
-import { Link } from 'react-router-dom';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 import JSImg from "../Images/js.png";
-import { Avatar } from '@mui/material';
+import { Avatar } from "@mui/material";
 
 const MainBody = () => {
-    return (
-        <>
-        <div className="mainBody">
-        
-       
-        <Card sx={{ maxWidth: 700}} className="mainBody--card">
-      <CardMedia
-        component="img"
-        height="300"
-        image={JSImg}
-        alt="green iguana"
-      />
-      <CardContent>
-      <div className='d-flex'>
-      <Avatar src={JSImg} className='mr-2' />
-        <Typography gutterBottom>
-        Tapas Adhikary <br /> jan 18
-        </Typography>
-        </div>
-      <Link to="/">
-        <Typography gutterBottom variant="h4">
-        JavaScript object destructuring usages you must know
-        </Typography>
-        </Link>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-        </div>
-            
-        </>
-    )
-}
+  return (
+    <>
+      <div className="mainBody">
+        <Card sx={{ maxWidth: 700 }} className="mainBody--card px-2">
+          <CardMedia
+            component="img"
+            height="300"
+            image={JSImg}
+            alt="green iguana"
+          />
+          <div className="d-flex m-md-2">
+            <Avatar src={JSImg} className="mr-2" />
+            <Typography>
+              Tapas Adhikary <br />
+              <small>jan 18</small>
+            </Typography>
+          </div>
 
-export default MainBody
+          <CardContent className="pl-md-5">
+            <Link to="/">
+              <Typography variant="h4">
+                JavaScript object destructuring usages you must know
+              </Typography>
+            </Link>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default MainBody;
