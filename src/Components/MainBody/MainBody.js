40e398b2f73438a3e1c9f30fee1b9454e6cfd44b
hicoderindia <hicoder.dolly@gmail.com>
2022-01-19 13:13:09 +0530

    Single Card completed

--- src/Components/MainBody/MainBody.js
+++ src/Components/MainBody/MainBody.js
@@ -8,21 +8,24 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import JSImg from "../Images/js.png";
+import user1 from "../Images/ben.jpg";
 import { Avatar } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
 const MainBody = () => {
   return (
     <>
       <div className="mainBody">
-        <Card sx={{ maxWidth: 700 }} className="mainBody--card px-2">
+        <Card sx={{ maxWidth: 700 }} className="mainBody--card">
           <CardMedia
             component="img"
             height="300"
             image={JSImg}
             alt="green iguana"
           />
-          <div className="d-flex m-md-2">
-            <Avatar src={JSImg} className="mr-2" />
+          <div className="d-flex m-md-3 mb-md-0">
+            <Avatar src={user1} className="mr-2" />
             <Typography>
               Tapas Adhikary <br />
               <small>jan 18</small>
@@ -31,19 +34,33 @@ const MainBody = () => {
 
           <CardContent className="pl-md-5">
             <Link to="/">
-              <Typography variant="h4">
+              <Typography className="mainBody--header">
                 JavaScript object destructuring usages you must know
               </Typography>
             </Link>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
+            <CardActions>
+              <button className="btn btn-sm mainBody--btn1">#javascript</button>
+              <button className="btn btn-sm mainBody--btn2">#programing</button>
+              <button className="btn btn-sm mainBody--btn3">#begginers</button>
+              <button className="btn btn-sm mainBody--btn4">#codenewbie</button>
+            </CardActions>
+            <CardActions className="d-flex justify-content-between">
+              <div>
+                {" "}
+                <button className="btn btn-sm">
+                  <FavoriteBorderIcon size="sm" /> #codenewbie
+                </button>
+                <button className="btn btn-sm">
+                  <ChatBubbleOutlineIcon size="sm" /> #codenewbie
+                </button>
+              </div>
+              <div>
+                {" "}
+                <button className="btn btn-sm">9 min read</button>
+                <button className="btn  mainBody--btn5">Save</button>
+              </div>
+            </CardActions>
           </CardContent>
-          <CardActions>
-            <Button size="small">Share</Button>
-            <Button size="small">Learn More</Button>
-          </CardActions>
         </Card>
       </div>
     </>
@@ -51,3 +68,18 @@ const MainBody = () => {
 };
 
 export default MainBody;
+
+const hastags = [
+  {
+    user: "../Image/ben.jpg",
+    name: "Matej Bačo",
+    date: "17 jan",
+    header: "Did we just build a Netflix clone with Appwrite?",
+    hash1: "javascript",
+    hash3: "opensource",
+    hash4: "Webdev",
+    hash5: "programing",
+    comment: "14",
+    readMin: "9 min",
+  },
+];
